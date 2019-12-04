3407b20737347f47083011709032b813858d86e0
Faisal Mir <55284277+faisalbmir@users.noreply.github.com>
2019-12-03 18:08:32 -0600

    Added the file for the water gun turret
    Unedited copy of the code borrowed from Jason Poel Smith at Makezine.

---
+++ motion_activated_turret.ino
@@ -0,0 +1,45 @@
+// Here is some sample code that you can use to start the project.
+
+int relayOne = 2;           // Relay connected to digital pin 2 
+int relayTwo = 5;           // Relay connected to digital pin 4
+int relayThree = 6;         // Relay connected to digital pin 6 
+int motionSensorPin = 13;   // Motion Sensor connected to digital pin 13 
+int motionSensorState = 0; 
+
+void setup() 
+{ 
+  pinMode(relayOne, OUTPUT);       // sets the digital pin 2 as output
+  pinMode(relayTwo, OUTPUT);       // sets the digital pin 4 as output 
+  pinMode(relayThree, OUTPUT);     // sets the digital pin 6 as output 
+  pinMode(motionSensorPin, INPUT); // sets the digital pin 13 as input
+  
+  Serial.begin(9600);
+}
+void loop() 
+{
+  motionSensorState = digitalRead(motionSensorPin); //read output of motion sensor
+  if (motionSensorState == HIGH) // if motion is detected, fire gun
+  { 
+    digitalWrite(relayTwo, HIGH); // turns the second relay on to fire the gun 
+    delay(1000); digitalWrite(relayOne, HIGH); // turns the first relay on to turn the turret 
+    delay(150); digitalWrite(relayOne, LOW); // turns the first relay off
+    delay(500); digitalWrite(relayThree, HIGH); // turns the third relay on to turn the turret back 
+    delay(300); digitalWrite(relayThree, LOW); // turns the third relay off 
+    delay(500); digitalWrite(relayOne, HIGH); // turns the first relay on to turn the turret 
+    delay(300); digitalWrite(relayOne, LOW); // turns the first relay off 
+    delay(500); digitalWrite(relayThree, HIGH); // turns the third relay on to turn the turret back 
+    delay(300); digitalWrite(relayThree, LOW); // turns the third relay off 
+    delay(500); digitalWrite(relayTwo, LOW); // turns the second relay off to stop firing the gun 
+    digitalWrite(relayOne, HIGH); // turns the first relay on to turn the turret all the way to one side 
+    delay(700); digitalWrite(relayOne, LOW); // turns the first relay off
+    delay(500); digitalWrite(relayThree, HIGH); // turns the third relay on to turn the turret back to the center position
+    delay(375); digitalWrite(relayThree, LOW); // turns the third relay off 
+    delay(1000); 
+  } 
+  else              // if no motion is detected, default to setting all motors off
+  { 
+    digitalWrite(relayOne, LOW); 
+    digitalWrite(relayTwo, LOW); 
+    digitalWrite(relayThree, LOW); 
+  }
+ }
